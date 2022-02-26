116b684c055341e613b4bea9f57781af193b4b51
NixonXC <81410474+NixonXC@users.noreply.github.com>
2022-02-26 00:28:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask
 import json, time
 import random
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 
+limiter = Limiter(app, key_func=get_remote_address)
+
 @app.route('/', methods=['GET'])
+@limiter.limit("120/minute")
 def home_page():
   lines = open('que.txt').read().splitlines()
   que = random.choice(lines)
